e67a326e163a88218d943467d26dded9120012d2
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-09-10 15:42:37 +0200

    waowie this works

--- src/main/java/org/polyfrost/soundtweaks/mixins/PositionedSoundMixin.java
+++ src/main/java/org/polyfrost/soundtweaks/mixins/PositionedSoundMixin.java
@@ -1,12 +1,13 @@
 package org.polyfrost.soundtweaks.mixins;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.audio.PositionedSound;
 import net.minecraft.util.ResourceLocation;
 import org.polyfrost.soundtweaks.SoundTweaks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PositionedSound.class)
 public abstract class PositionedSoundMixin {
@@ -14,15 +15,15 @@ public abstract class PositionedSoundMixin {
     @Shadow
     public abstract ResourceLocation getSoundLocation();
 
-    @ModifyReturnValue(method = "getVolume", at = @At("RETURN"))
-    private float soundtweaks$modifyVolume(float original) {
+    @Inject(method = "getVolume", at = @At("RETURN"), cancellable = true)
+    private void soundtweaks$modifyVolume(CallbackInfoReturnable<Float> cir) {
         Float modifier = SoundTweaks.Companion.getVolumes().get(getSoundLocation());
 
         if (modifier == null || modifier == 100.0f) {
-            return original;
+            return;
         }
 
-        return original * (modifier / 100);
+        cir.setReturnValue(cir.getReturnValueF() * (modifier / 100));
     }
 
 }
