1fc7dd61b34bcae24e0a0deda642b82b1d83cc0e
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-09-11 13:57:04 +0200

    die intellij

--- src/main/kotlin/org/polyfrost/soundtweaks/SoundTweaks.kt
+++ src/main/kotlin/org/polyfrost/soundtweaks/SoundTweaks.kt
@@ -3,12 +3,12 @@ package org.polyfrost.soundtweaks
 //#if FABRIC
 //$$ import net.fabricmc.api.ModInitializer;
 //#elseif FORGE
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
 //#endif
 import dev.deftu.omnicore.client.OmniClient.getInstance
 import net.minecraft.client.audio.SoundEventAccessorComposite
 import net.minecraft.util.ResourceLocation
-import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import org.polyfrost.soundtweaks.config.SoundTweaksConfig
 import org.polyfrost.soundtweaks.mixins.RegistrySimpleAccessor
 import org.polyfrost.soundtweaks.mixins.SoundHandlerAccessor
